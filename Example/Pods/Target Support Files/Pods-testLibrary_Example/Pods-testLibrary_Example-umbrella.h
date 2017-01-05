23553e84f528712203c0e24d3abe9d36866f9155
Trupti Adalapure <truptiadalapure@Truptis-MacBook-Pro.local>
2017-01-05 18:24:11 +0530

    Create bare project via poad create lib.

---
+++ Example/Pods/Target Support Files/Pods-testLibrary_Example/Pods-testLibrary_Example-umbrella.h
@@ -0,0 +1,8 @@
+#ifdef __OBJC__
+#import <UIKit/UIKit.h>
+#endif
+
+
+FOUNDATION_EXPORT double Pods_testLibrary_ExampleVersionNumber;
+FOUNDATION_EXPORT const unsigned char Pods_testLibrary_ExampleVersionString[];
+
---
+++ Example/Pods/Target Support Files/testLibrary/testLibrary-dummy.m
@@ -0,0 +1,5 @@
+#import <Foundation/Foundation.h>
+@interface PodsDummy_testLibrary : NSObject
+@end
+@implementation PodsDummy_testLibrary
+@end
